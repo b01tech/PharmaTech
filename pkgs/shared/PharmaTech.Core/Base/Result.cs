c5589b775a0848d7a94bacfc55c764ef514ecb26
b01tech <bruno01.tech@gmail.com>
2026-02-23 17:35:36 -0300

    feat(core): implement result pattern classes

---
+++ pkgs/shared/PharmaTech.Core/Base/Result.cs
@@ -0,0 +1,33 @@
+﻿namespace PharmaTech.Core.Base;
+
+public class Result
+{
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public IList<string> Errors { get; }
+
+    protected Result(bool isSuccess, IList<string> errors)
+    {
+        IsSuccess = isSuccess;
+        Errors = new List<string>(errors);
+    }
+
+    public static Result Success() => new Result(true, []);
+    public static Result Failure(IList<string> errors) => new Result(false, errors);
+    public static Result Failure(string error) => new Result(false, [error]);
+}
+
+public class Result<T> : Result
+{
+    public T Data { get; } = default!;
+
+    private Result(bool isSuccess, IList<string> errors, T data) : base(isSuccess, errors)
+    {
+        Data = data;
+    }
+    public static Result<T> Success(T data) => new Result<T>(true, [], data);
+    public new static Result<T> Failure(IList<string> errors) => new(false, errors, default!);
+    public new static Result<T> Failure(string error) => new(false, [error], default!);
+
+    public static implicit operator Result<T>(T data) => Success(data);
+}
