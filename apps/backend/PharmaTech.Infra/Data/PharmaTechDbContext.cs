d56e1432700d41e2e06d5ad44f1c91518a4f5322
b01tech <bruno01.tech@gmail.com>
2026-02-24 09:10:14 -0300

    feat(infra): add PharmaTechDbContext

---
+++ apps/backend/PharmaTech.Infra/Data/PharmaTechDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PharmaTech.Product.Category.Models;
+
+namespace PharmaTech.Infra.Data;
+
+public class PharmaTechDbContext(DbContextOptions<PharmaTechDbContext> options) : DbContext(options)
+{
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Subcategory> Subcategories { get; set; } = null!;
+    public DbSet<Product.Product.Models.Product> Products { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(PharmaTechDbContext).Assembly);
+    }
+}
