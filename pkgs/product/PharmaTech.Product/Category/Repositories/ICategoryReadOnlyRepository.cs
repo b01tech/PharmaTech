376703e66e38771c69d29f60c0791d6419d3be5b
b01tech <bruno01.tech@gmail.com>
2026-02-23 22:23:56 -0300

    feat(category): add read-only and write repository interfaces

---
+++ pkgs/product/PharmaTech.Product/Category/Repositories/ICategoryReadOnlyRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace PharmaTech.Product.Category.Repositories;
+
+public interface ICategoryReadOnlyRepository
+{
+    Task<Models.Category> GetCategoryByIdAsync(Guid id);
+    Task<IEnumerable<Models.Category>> GetAllCategoriesAsync(int page = 1, int pageSize = 25);
+    Task<Models.Category> GetAllSubCategoriesAsync(Guid categoryId);
+}
---
+++ pkgs/product/PharmaTech.Product/Category/Repositories/ICategoryWriteRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace PharmaTech.Product.Category.Repositories;
+
+public interface ICategoryWriteRepository
+{
+    Task CreateAsync(Models.Category category);
+    Task UpdateAsync(Models.Category category);
+    Task RemoveAsync(Guid id);
+    Task CreateSubCategoryAsync(Models.Subcategory subcategory);
+    Task RemoveSubCategoryAsync(Guid id);
+}
