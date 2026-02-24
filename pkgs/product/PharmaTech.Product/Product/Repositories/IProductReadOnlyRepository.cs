4ca0f9126389efc13459e3ecbf36eae6aa57bf07
b01tech <bruno01.tech@gmail.com>
2026-02-23 22:24:42 -0300

    feat(product): add product repository interfaces

---
+++ pkgs/product/PharmaTech.Product/Product/Repositories/IProductReadOnlyRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace PharmaTech.Product.Product.Repositories;
+
+public interface IProductReadOnlyRepository
+{
+    Task<Models.Product?> GetProductByIdAsync(Guid id);
+
+    Task<IEnumerable<Models.Product>> GetAllProductsWithFilterAsync(Delegate filter, Guid? categoryId = null,
+        Guid? subcategoryId = null, int page = 1, int pageSize = 25);
+    Task<(int TotalProducts, int TotalCategories, int TotalSubcategories)> GetTotalAsync();
+}
---
+++ pkgs/product/PharmaTech.Product/Product/Repositories/IProductWriteRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace PharmaTech.Product.Product.Repositories;
+
+public interface IProductWriteRepository
+{
+    Task CreateProductAsync(Models.Product product);
+    Task UpdateProductAsync(Models.Product product);
+    Task DeleteProductAsync(Models.Product product);
+}
