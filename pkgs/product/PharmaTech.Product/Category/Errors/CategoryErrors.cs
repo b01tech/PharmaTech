2429615896ae7a1adb548afcb69cfe1a111d82f3
b01tech <bruno01.tech@gmail.com>
2026-02-23 22:21:56 -0300

    feat: add error constants for product and category

---
+++ pkgs/product/PharmaTech.Product/Category/Errors/CategoryErrors.cs
@@ -0,0 +1,11 @@
+﻿namespace PharmaTech.Product.Category.Errors;
+
+public record CategoryErrors
+{
+    public const string NotFound = "CATEGORY_NOT_FOUND";
+}
+
+public record SubCategoryErrors
+{
+    public const string NotFound = "SUB_CATEGORY_NOT_FOUND";
+}
---
+++ pkgs/product/PharmaTech.Product/Product/Errors/ProductErrors.cs
@@ -0,0 +1,6 @@
+﻿namespace PharmaTech.Product.Product.Errors;
+
+public record ProductErrors
+{
+    public const string NotFound = "PRODUCT_NOT_FOUND";
+};
