d04462c5966e92eebc866a1e687de30bac2d01ca
b01tech <bruno01.tech@gmail.com>
2026-02-24 09:11:03 -0300

    feat(infra): add database infrastructure and service registration

--- apps/backend/PharmaTech.Api/Program.cs
+++ apps/backend/PharmaTech.Api/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
 using PharmaTech.Api.Extensions;
+using PharmaTech.Infra.Data;
+using PharmaTech.Infra.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddApiDocumentation();
+
+builder.Services
+    .AddApiDocumentation()
+    .AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ apps/backend/PharmaTech.Infra/Extensions/InfrastructureExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PharmaTech.Infra.Data;
+
+namespace PharmaTech.Infra.Extensions;
+
+public static class InfrastructureExtension
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddContext(services, configuration);
+        return services;
+    }
+
+    private static void AddContext(IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+        services.AddDbContext<PharmaTechDbContext>(options =>
+            options.UseNpgsql(connectionString));
+    }
+}
