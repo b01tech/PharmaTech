d232ae2fc7089549dba1df2f72ab44fbdb8bae7c
b01tech <bruno01.tech@gmail.com>
2026-02-23 22:18:55 -0300

    refactor: improve Result class formatting and add MergeErrors method

--- pkgs/shared/PharmaTech.Core/Base/Result.cs
+++ pkgs/shared/PharmaTech.Core/Base/Result.cs
@@ -13,21 +13,29 @@ public class Result
     }
 
     public static Result Success() => new Result(true, []);
+
     public static Result Failure(IList<string> errors) => new Result(false, errors);
+
     public static Result Failure(string error) => new Result(false, [error]);
+
+    public static IList<string> MergeErrors(params Result[] results) => results.SelectMany(r => r.Errors).ToList();
 }
 
 public class Result<T> : Result
 {
     public T Data { get; } = default!;
 
-    private Result(bool isSuccess, IList<string> errors, T data) : base(isSuccess, errors)
+    private Result(bool isSuccess, IList<string> errors, T data)
+        : base(isSuccess, errors)
     {
         Data = data;
     }
+
     public static Result<T> Success(T data) => new Result<T>(true, [], data);
-    public new static Result<T> Failure(IList<string> errors) => new(false, errors, default!);
-    public new static Result<T> Failure(string error) => new(false, [error], default!);
+
+    public static new Result<T> Failure(IList<string> errors) => new(false, errors, default!);
+
+    public static new Result<T> Failure(string error) => new(false, [error], default!);
 
     public static implicit operator Result<T>(T data) => Success(data);
 }
