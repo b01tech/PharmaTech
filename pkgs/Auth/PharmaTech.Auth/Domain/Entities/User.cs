eb97d696bee102d5529633dfde9be9cfd4226a8d
b01tech <bruno01.tech@gmail.com>
2026-04-29 15:58:35 -0300

    feat(auth): add User entity

---
+++ pkgs/Auth/PharmaTech.Auth/Domain/Entities/User.cs
@@ -0,0 +1,58 @@
+using PharmaTech.Shared.Core;
+using PharmaTech.Shared.ValueObjects;
+
+namespace PharmaTech.Auth.Domain.Entities;
+
+public class User : Entity
+{
+    #region Properties
+    public Name Name { get; private set; }
+    public Email Email { get; private set; }
+    public Role Role { get; private set; }
+    public string? AvatarUrl { get; private set; }
+    #endregion
+
+    #region  Constructors
+    // EF Core
+    private User() { }
+
+    private User(Name name, Email email, Role role, string? avatarUrl = null)
+    {
+        Name = name;
+        Email = email;
+        Role = role;
+        AvatarUrl = avatarUrl;
+    }
+
+    public static Result<User> Create(string nameInput, string emailInput, Role role, string? avatarUrl = null)
+    {
+        var nameResult = Name.Create(nameInput);
+        var emailResult = Email.Create(emailInput);
+        if (nameResult.IsFailure || emailResult.IsFailure)
+            return Result<User>.Failure(Result.MergeErrors(nameResult, emailResult));
+
+        return new User(nameResult.Data, emailResult.Data, role, avatarUrl);
+    }
+    #endregion
+
+    #region Methods
+    public Result Update(string? nameInput = null, string? emailInput = null, string? avatarUrl = null)
+    {
+        var nameResult = Name.Create(nameInput ?? Name.Value);
+        var emailResult = Email.Create(emailInput ?? Email.Address);
+        if (nameResult.IsFailure || emailResult.IsFailure)
+            return Result<User>.Failure(Result.MergeErrors(nameResult, emailResult));
+
+        Name = nameResult.Data;
+        Email = emailResult.Data;
+        AvatarUrl = avatarUrl;
+        return Result.Success();
+    }
+
+    public Result UpdateRole(Role role)
+    {
+        Role = role;
+        return Result.Success();
+    }
+    #endregion
+}
