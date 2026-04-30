46d09d046773b645cdbf8693ddcb32aa41c40ccc
b01tech <bruno01.tech@gmail.com>
2026-04-30 10:27:39 -0300

    feat(infra): add repository registrations to DI container

--- pkgs/Auth/src/Auth.Infrastructure/DependencyInjection.cs
+++ pkgs/Auth/src/Auth.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
+using Auth.Domain.Repositories;
 using Auth.Domain.Services;
+using Auth.Infrastructure.Persistence.Repositories;
 using Auth.Infrastructure.Services;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,6 +10,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        AddRepositories(services);
         AddServices(services);
         return services;
     }
@@ -15,5 +18,13 @@ public static class DependencyInjection
     private static void AddServices(IServiceCollection services)
     {
         services.AddScoped<IEncrypter, Encrypter>();
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+        services.AddScoped<IUserWriteRepository, UserRepository>();
+        services.AddScoped<IPasswordRepository, PasswordRepository>();
+        services.AddScoped<IRoleRepository, RoleRepository>();
     }
 }
